49908d51ce34389efa83bb072f144eda4f81a28b
Alexandre Gonzalo <alexandre.gonzalo@agito.cc>
2016-03-13 20:17:21 +0700

    Initial Commit

---
+++ Virtual Tourist 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist 2
+//
+//  Created by Alexandre Gonzalo on 13/3/2016.
+//  Copyright © 2016 Agito Cloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
